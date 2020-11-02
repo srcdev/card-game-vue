7d9e40b28d045fa31015381b5d81fb34861ba8d7
Simon Cornforth <simon.cornforth@srcdev.co.uk>
2020-11-02 22:23:39 +0000

    Layout updated

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,20 @@
 <template>
   <div class="wrapper">
-    <header class="header"><p>Fill in the blanks game</p></header>
-    <aside class="aside questions-card-deck"><p>Question card displayed here</p></aside>
+    <header class="header">
+      <p>Fill in the blanks game</p>
+    </header>
+    <aside class="aside questions-card-deck">
+      <p>Question card displayed here</p>
+    </aside>
     <div class="answer-cards-deck">
-      <h1 class="header1" data-test="h1-text">Fill in the blanks game</h1>
-      <h1 class="header1" data-test="h1-text">&lt;h1 class="header1"&gt;Header 1 text.&lt;/h1&gt;</h1>
-      <p class="header1 header1__light">&lt;p class="header1 header1__light"&gt;Header 1 text.&lt;/p&gt;</p>
-      <h2 class="header2">&lt;h2 class="header2"&gt;Header 2 text.&lt;/h2&gt;</h2>
-      <p class="header2 header2__light">&lt;p class="header2 header2__light"&gt;Header 2 text.&lt;/p&gt;</p>
-      <h3 class="header3">&lt;h3 class="header3"&gt;Header 3 text.&lt;/h3&gt;</h3>
-      <p class="header3 header3__light">&lt;p class="header3 header1__light"&gt;Header 3 text.&lt;/p&gt;</p>
-      <h4 class="header4">&lt;h4 class="header4"&gt;Header 4 text.&lt;/h4&gt;</h4>
-      <p class="header4 header4__light">&lt;p class="header4 header4__light"&gt;Header 4 text.&lt;/p&gt;</p>
-      <h5 class="header5">&lt;h5 class="header5"&gt;Header 5 text.&lt;/h5&gt;</h5>
-      <p class="header5 header5__light">&lt;p class="header5 header5__light"&gt;Header 5 text.&lt;/p&gt;</p>
-      <h6 class="header6">&lt;h6 class="header1"&gt;Header 6 text.&lt;/h6&gt;</h6>
-      <p class="header6 header6__light">&lt;p class="header6 header6__light"&gt;Header 6 text.&lt;/p&gt;</p>
-      <p data-test="message" class="message">{{ infoText }}</p>
+      <p>Answers cards here</p>
     </div>
-    <aside class="aside game-status-deck"><p>Game status here</p></aside>
-    <footer class="footer"><p>Footer</p></footer>
+    <aside class="aside game-status-deck">
+      <p>Game status here</p>
+    </aside>
+    <footer class="footer">
+      <p>Footer</p>
+    </footer>
   </div>
 </template>
 
