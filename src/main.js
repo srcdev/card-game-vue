bba3f08250a1b94870cba99eb2aaefa35728565e
Simon Cornforth <simon.cornforth@srcdev.co.uk>
2020-11-03 00:16:19 +0000

    Add vue router

--- src/main.js
+++ src/main.js
@@ -1,12 +1,20 @@
 import Vue from 'vue'
 import Vuex from "vuex";
+import VueRouter from 'vue-router';
 import App from './App.vue'
 import store from "./store/index";
 
 Vue.config.productionTip = false
 Vue.use(Vuex);
 
+Vue.use(VueRouter);
+const router = new VueRouter({
+  mode: 'history',
+  routes: [],
+});
+
 new Vue({
+  router,
   store,
   render: h => h(App)
 }).$mount("#app");
