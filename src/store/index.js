8c4500cb55fa9da8de96301bd6d0300afa260571
Simon Cornforth <simon.cornforth@srcdev.co.uk>
2020-11-04 11:27:49 +0000

    Added vuex-persist and localforage for state persistance

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,23 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import VuexPersist from 'vuex-persist';
+import localForage from 'localforage';
 import game from "./modules/game/index";
 
 Vue.use(Vuex);
 
+const vuexStorage = new VuexPersist({
+  key: 'persistedGameData',
+  storage: localForage,
+//  storage: window.sessionStorage,
+  asyncStorage: true,
+  reducer: state => ({
+    gameId: state.game.gameId,
+  })    
+})
+
 export default new Vuex.Store({
+  plugins: [vuexStorage.plugin],
   modules: {
     game
   },
