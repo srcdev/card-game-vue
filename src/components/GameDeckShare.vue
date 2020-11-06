3458a2683de50690561d5247d1b0952394cb1c51
Simon Cornforth <simon.cornforth@srcdev.co.uk>
2020-11-06 23:58:33 +0000

    Added QR Share code

---
+++ src/components/GameDeckShare.vue
@@ -0,0 +1,64 @@
+<template>
+  <div v-if="hrefBuilt" class="body__section__inner">
+    <h3 class="header-3">Scan QR or share link</h3>
+    <p><a :href="gameHref" class="sharelink">{{ gameHref }}</a></p>
+    <div
+      class="sharepanel"
+      >
+      <qrcode-vue :value="gameHref" size="260" level="H" class="shareQr"></qrcode-vue>
+    </div>
+  </div>
+</template>
+
+<script>
+  // import { mapState } from 'vuex';
+  import QrcodeVue from 'qrcode.vue';
+  export default {
+    name: "GameDeckShare",
+    components: {
+      QrcodeVue
+    },
+    data () {
+      return {
+        hrefBuilt: false,
+      }
+    },
+    created() {
+      this.setGameHref();
+    },
+    methods: {
+      setGameHref() {
+        this.gameHref = `${document.location.href}`;
+        this.hrefBuilt = true;
+      }
+    }
+  };
+</script>
+
+<style lang="scss" scoped>
+  @import "../styles/imports";
+
+  .body__section__inner {
+    .sharelink {
+      color: $white;
+      font-size: 10px;
+      font-weight: normal;
+    }
+
+    .sharepanel {
+      display: flex;
+      margin: 12px 0px;
+      .shareQr {
+        background-color: $white;
+        display: inline-block;
+        margin: 0 auto;
+        padding: 8px;
+      }
+    }
+  }
+
+  @media (prefers-color-scheme: dark) {
+
+  }
+
+</style>
--- src/components/GameDeckStartWaiting.vue
+++ src/components/GameDeckStartWaiting.vue
@@ -1,24 +1,40 @@
 <template>
-    <div class="game-deck-start">
-        <h2 class="header2">Waiting to join game</h2>
-        <p>Join Game "{{ gameName }}", id: {{ gameId }}</p>
-        <ul>
-            <li v-for="(player, index) in gameData.players" :key="`user-${index}`">{{ player.playerName }}</li>
-        </ul>
-    </div>
+  <div class="game-deck-start">
+    <h2 class="header2">Waiting to join game</h2>
+    <p>Join Game "{{ gameName }}", id: {{ gameId }}</p>
+    <ul>
+      <li v-for="(player, index) in gameData.players" :key="`user-${index}`">{{ player.playerName }}</li>
+    </ul>
+    <game-deck-share />
+  </div>
 </template>
 
 <script>
   import { mapState } from 'vuex';
+  import GameDeckShare from './GameDeckShare'
   export default {
     name: "GameDeckStartWaiting",
+    components: {
+      'game-deck-share': GameDeckShare,
+    },
     computed: {
       ...mapState('game', [
-          'gameData',
-          'gameId',
-          'gameName',
-          'playerIsDealer',
+        'gameData',
+        'gameId',
+        'gameName',
+        'playerIsDealer',
       ])
     },
+    created() {
+      if (this.$route.query.gameId === undefined) {
+        this.setRoute();
+      }
+    },
+    methods: {
+      setRoute() {
+        const gameQuery = {gameId: this.gameId};
+        this.$router.replace({ query: gameQuery })
+      }
+    }
   }
 </script>
