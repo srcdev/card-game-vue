4eba070b5d3edc1f1371e028e82e3f824ac53196
Simon Cornforth <simon.cornforth@srcdev.co.uk>
2020-11-07 00:41:06 +0000

    Swap to session storage, get shot of localForage, its not needed

--- src/store/index.js
+++ src/store/index.js
@@ -1,24 +1,17 @@
 import Vue from "vue";
 import Vuex from "vuex";
 import VuexPersist from 'vuex-persist';
-import localForage from 'localforage';
 import game from "./modules/game/index";
 
 Vue.use(Vuex);
 
-const vuexStorage = new VuexPersist({
-  key: 'persistedGameData',
-  storage: localForage,
-  asyncStorage: true,
-  // reducer: state => ({
-  //   gameId: state.game.gameId,
-  //   gameState: state.game.gameState,
-  //   playerIsDealer: state.game.playerIsDealer,
-  // })    
+const vuexLocalStorage = new VuexPersist({
+  key: 'vuex',
+  storage: window.sessionStorage,
 })
 
 export default new Vuex.Store({
-  plugins: [vuexStorage.plugin],
+  plugins: [vuexLocalStorage.plugin],
   modules: {
     game
   },
