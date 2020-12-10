61ad3560b090742ce311fe17b655d2fcea9d8b22
Simon Cornforth <simon.cornforth@srcdev.co.uk>
2020-12-10 00:09:34 +0000

    List rounds latest first (reverse object keys) and UI tweaks

--- src/components/GameDeckRounds.vue
+++ src/components/GameDeckRounds.vue
@@ -6,11 +6,11 @@
       class="game-deck__rounds"
       >
       <div
-        v-for="(round, roundIndex) in rounds"
+        v-for="(round, roundIndex) in reveresedRounds"
         :key="roundIndex"
         class="game-deck__round"
       >
-        <p class="game-deck__header">Round: {{ roundIndex }}</p>
+        <p class="game-deck__header">Round: {{ round.key }}</p>
 
         <div class="game-deck-answers">
           <div class="shim shim__left"></div>
@@ -22,18 +22,20 @@
                 :key="answerIndex"
                 class="game-deck-cards-item"
               >
-                <p class="game-deck__header">
-                  Player: {{ answer.playerName }}
-                  <span v-if="answer.isWinner">(Winner)</span>
-                </p>
-                <game-deck-card
-                  :qa-data="{
-                    id: answerIndex,
-                    data: answer
-                  }"
-                  card-size="L"
-                  card-type="QA"
-                />
+                <div v-if="typeof answer === 'object'">
+                  <p class="game-deck__header">
+                    {{ answer.playerName }}
+                    <span v-if="answer.isWinner">(Winner)</span>
+                  </p>
+                  <game-deck-card
+                    :qa-data="{
+                      id: answerIndex,
+                      data: answer
+                    }"
+                    card-size="L"
+                    card-type="QA"
+                  />
+                </div>
               </li>
             </ul>
           </div>
@@ -61,21 +63,37 @@
       ]),
       hasRounds() {
         return typeof this.rounds === 'object';
+      },
+      reveresedRounds() {
+        return Object.keys(this.rounds).sort().reverse().map(key=> ({ ...this.rounds[key], key }) );
       }
     },
   }
 </script>
 
 <style lang="scss" scoped>
+  .header1 {
+    text-align: center;
+  }
   .game-deck {
-
+    $self: &;
     &__rounds {
+      max-width: 500px;
 
+      @include breakpoint(768) {
+        margin: 0 auto;
+      }
+
+      #{ $self }-answers {
+        padding-top: 0;
+      }
     }
 
     &__header {
-      margin: 12px auto;
+      font-weight: bold;
+      margin: 0 auto;
       text-align: center;
+      text-decoration: underline;
     }
 
     &-cards-item {
