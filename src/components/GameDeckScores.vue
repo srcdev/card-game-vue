3bc81e855d5bcd381ee25bfa0fed98d31750b194
Simon Cornforth <simon.cornforth@srcdev.co.uk>
2020-12-11 20:36:15 +0000

    Added cards remaining in deck to scores

--- src/components/GameDeckScores.vue
+++ src/components/GameDeckScores.vue
@@ -1,20 +1,26 @@
 <template>
-  <div class="game-deck">
-    <h3 class="header-3">Scores</h3>
+  <div class="game-deck game-deck__stats">
+    <h3 class="header-3">Game stats</h3>
+    <p class="header-3">Cards left in deck</p>
 
-    <ul class="player-list">
-      <li
+    <dl class="dl-list">
+      <dt class="dt">Questions: </dt>
+      <dd class="dd">{{ this.questionsInGame }}</dd>
+      <dt class="dt">Answers: </dt>
+      <dd class="dd">{{ this.answersInGame }}</dd>
+    </dl>
+
+    <p class="header-3">Scores</p>
+
+    <dl class="dl-list">
+      <template
         v-for="(player, index) in scoresObj"
-        :key="index"
         class="player-list-item"
         >
-        <span
-          class="player-list-item-name"
-          >
-          {{ player.playerName }} ({{ player.winCount }})
-        </span>
-      </li>
-    </ul>
+        <dt :key="index" class="dt">{{ player.playerName }}: </dt>
+        <dd :key="index" class="dd">{{ player.winCount }}</dd>
+      </template>
+    </dl>
 
   </div>
 </template>
@@ -25,8 +31,26 @@
     name: "GameDeckScores",
     computed: {
       ...mapState('game', [
+        'answersInGame',
+        'questionsInGame',
         'scoresObj',
       ])
     },
   };
 </script>
+
+<style lang="scss">
+
+  .game-deck {
+    &__stats {
+      .dl-list {
+        border-top: 1px solid $border-light;
+        padding-top: 8px;
+        @media (prefers-color-scheme: dark) {
+          border-top: 1px solid $border-dark;
+        }
+      }
+    }
+  }
+
+</style>
