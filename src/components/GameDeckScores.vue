2a758f3c512a7cb2a2da5bdfdf932e4f9bc0a881
Simon Cornforth <simon.cornforth@srcdev.co.uk>
2020-12-13 17:46:29 +0000

    UI update in Scores component

--- src/components/GameDeckScores.vue
+++ src/components/GameDeckScores.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="game-deck game-deck__stats">
     <h3 class="header-3">Game stats</h3>
-    <p class="header-3">Cards left in deck</p>
+    <p class="header-4">Cards left in deck</p>
 
     <dl class="dl-list">
       <dt class="dt">Questions: </dt>
@@ -10,17 +10,22 @@
       <dd class="dd">{{ this.answersInGame }}</dd>
     </dl>
 
-    <p class="header-3">Scores</p>
+    <p class="header-4">Scores</p>
 
-    <dl class="dl-list">
-      <template
+    <table class="scores-table">
+      <thead class="scores-table__th">
+        <td class="scores-table__td">Player name</td>
+        <td class="scores-table__td">Score</td>
+      </thead>
+      <tr
         v-for="(player, index) in scoresObj"
-        class="player-list-item"
-        >
-        <dt :key="index" class="dt">{{ player.playerName }}: </dt>
-        <dd :key="index" class="dd">{{ player.winCount }}</dd>
-      </template>
-    </dl>
+        class="scores-table__tr"
+        :key="index"
+      >
+        <td class="scores-table__td">{{ player.playerName }}: </td>
+        <td class="scores-table__td">{{ player.winCount }}</td>
+      </tr>
+    </table>
 
   </div>
 </template>
@@ -43,11 +48,46 @@
 
   .game-deck {
     &__stats {
-      .dl-list {
-        border-top: 1px solid $border-light;
-        padding-top: 8px;
+
+      .header-3 {
+        margin-bottom: 12px;
+      }
+
+      .header-4 {
+        font-weight: bold;
+        border-bottom: 1px solid $border-light;
+        padding-bottom: 12px;
         @media (prefers-color-scheme: dark) {
-          border-top: 1px solid $border-dark;
+          border-bottom: 1px solid $border-dark;
+        }
+      }
+      .dl-list {
+        margin-top: 12px;
+      }
+
+      .scores-table {
+        margin-top: 8px;
+        padding-top: 8px;
+        width: 100%;
+
+        &__th,
+        &__tr {
+          display: flex;
+          flex-flow: row wrap;
+          margin-bottom: 12px;
+        }
+        &__th td {
+          font-weight: bold;
+        }
+        &__td {
+          flex-basis: 50px;
+          padding: 4px 0;
+
+          &:first-child {
+            flex-basis: calc(100% - 50px);
+            flex-grow: 1;
+            padding: 4px 0;
+          }
         }
       }
     }
